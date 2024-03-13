e3cc345a515ecfc07c06666101181c615fe836e0
Mohamed-Salah-1 <m.salah2405@gmail.com>
2024-03-13 23:55:19 +0300

    first commit

---
+++ lecture3/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('hello/', include('hello.urls')),
+    path('newyear/', include('newyear.urls')),
+
+]
